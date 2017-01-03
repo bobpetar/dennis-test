5006874a5943723e7eaa71cea25fd140162c15ca
Risteski Petar <bobpetar@gmail.com>
2017-01-03 21:09:25 +0100

    create video post from youtube link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103183708) do
+ActiveRecord::Schema.define(version: 20170103193108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,5 +42,15 @@ ActiveRecord::Schema.define(version: 20170103183708) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id", using: :btree
+  end
+
   add_foreign_key "images", "users"
+  add_foreign_key "videos", "users"
 end
